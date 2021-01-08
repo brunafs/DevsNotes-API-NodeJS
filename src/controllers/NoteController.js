5dc2aa823adf2b982941635f5e7df614e562c997
Bruna Santana <brunasantana98@gmail.com>
2021-01-07 21:05:22 -0300

    Estrutura MVC

---
+++ src/controllers/NoteController.js
@@ -0,0 +1,7 @@
+const NoteSerive = require('../services/NoteService');
+
+module.exports = {
+   ping: (req, res) => {
+      res.json({pong: true});
+   }
+};
--- src/server.js
+++ src/server.js
@@ -3,13 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const routes = require('./routes');
+
 const server = express();  //criar servidor
 server.use(cors());        //requisitar api de qualquer dispositivo
 server.use(bodyParser.urlencoded({extended: false})); //ajustar dados
-
-server.get('/ping', (req, res)=>{
-   res.json({pong: true});
-});
+server.use('/api/', routes);
 
 server.listen(process.env.PORT, () => {
    console.log(`Servidor rodabndo em: http://localhost:${process.env.PORT}`);
