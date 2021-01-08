5c92b5c2790d2de46c3025dac3b757d524b27855
Bruna Santana <brunasantana98@gmail.com>
2021-01-07 21:33:58 -0300

    Conexao Banco de dados + estrurando rotas

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -3,5 +3,21 @@ const NoteSerive = require('../services/NoteService');
 module.exports = {
    ping: (req, res) => {
       res.json({pong: true});
+   },
+   all: () => {
+
+   },
+   one: () => {
+
+   },
+   new: () => {
+
+   },
+   edit: () => {
+
+   },
+   delete: () => {
+
    }
 };
+
--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,15 @@ const NoteController = require('./controllers/NoteController');
 
 router.get('/ping', NoteController.ping);
 
+// GET /api/notes -> Listar todas notas
+router.get('/notes', NoteController.all);
+// GET /api/note/id -> Exibe uma nota
+router.get('/note/:id', NoteController.one);
+// POST /api/note -> Adicionar uma nota
+router.post('/note', NoteController.new);
+//PUT /api/note/id -> Atualizar uma nota
+router.put('/note/:id', NoteController.edit);
+//DELETE /api/note/id -> Deletar uma nota
+router.delete('/note/:id', NoteController.delete);
+
 module.exports = router;
