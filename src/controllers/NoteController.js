110dc5a869fb1887560673a2738aab0e832b079b
Bruna Santana <brunasantana98@gmail.com>
2021-01-07 22:25:59 -0300

    Rotas/Endpoints funcionando

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -4,20 +4,67 @@ module.exports = {
    ping: (req, res) => {
       res.json({pong: true});
    },
-   all: () => {
-
+   all: async (req, res) => {
+      let json = {error:'', result:[]};
+      let notes = await NoteSerive.getAll();
+      for(let i in notes){
+         json.result.push({
+            id:notes[i].id,
+            title: notes[i].title
+         });
+      }
+      res.json(json);
    },
-   one: () => {
+   one: async (req, res) => {
+      let json = {error:'', result:{}};
+      let id = req.params.id;
+      let note = await NoteSerive.findById(id);
+      if(note){
+         json.result = note;
+      }
 
+      res.json(json);
    },
-   new: () => {
-
+   new: async (req, res) => {
+      let json = {error:'', result:{}};
+      let title = req.body.title;
+      let body =  req.body.body;
+      if(title && body){
+         let noteId = await NoteSerive.add(title, body);
+         json.result = {
+            id: noteId,
+            title,
+            body
+         };
+      }else {
+         json.error = 'Campos não enviados.';
+      }
+      res.json(json);
    },
-   edit: () => {
+   edit: async (req, res) => {
+      let json = {error:'', result:{}};
 
+      let id = req.params.id;
+      let title = req.body.title;
+      let body =  req.body.body;
+
+      if(title && body){
+         await NoteSerive.update(id, title, body);
+
+         json.result = {
+            id,
+            title,
+            body
+         };
+      }else {
+         json.error = 'Campos não enviados.';
+      }
+      res.json(json);
    },
-   delete: () => {
-
+   delete: async (req, res) => {
+      let json = {error:'', result:{}};
+      await NoteSerive.delete(req.params.id);
+      res.json(json);
    }
 };
 
